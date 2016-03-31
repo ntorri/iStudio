cd425f2c8750cd0618d9ef1da2f8878c38b59f1a
Nate Torri <ntorri@nates-macbook-pro.local>
2016-03-31 12:46:33 -0600

    Initial Commit

---
+++ iStudio/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iStudio
+//
+//  Created by Nate Torri on 3/31/16.
+//  Copyright © 2016 Nate Torri. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iStudio/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iStudio
+//
+//  Created by Nate Torri on 3/31/16.
+//  Copyright © 2016 Nate Torri. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
